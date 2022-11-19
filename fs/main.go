3f5dfde3e7635a0cb4c6314f6a369982e5350996
Ronald G. Minnich <rminnich@gmail.com>
2022-11-18 16:39:23 -0800

    get ready to move forth into a package.
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- fs/main.go
+++ fs/main.go
@@ -7,83 +7,11 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/fs"
 	"log"
 	"os"
 
 	"github.com/u-root/u-root/pkg/forth"
-	"github.com/usbarmory/tamago-example/t9"
-	"harvey-os.org/ninep/protocol"
 )
-
-var (
-	v = log.Printf
-)
-
-// The great panic discussion.
-// Rob has given talks on using panic for parsers.
-// I have talked to Russ about using panic for parsers.
-// Short form: it's ok. In general, don't panic.
-// But parsers are special: using panic
-// in a parser makes the code tons cleaner.
-
-// Note that if any type asserts fail the forth interpret loop catches
-// it. It also catches stack underflow, all that stuff.
-func open(f forth.Forth) {
-	forth.Debug("open")
-	g := f.Pop().(string)
-	forth.Debug("%v", g)
-	r := f.Pop().(t9.FS)
-	c, err := r.Open(g)
-	if err != nil {
-		panic(fmt.Sprintf("%v", err))
-	}
-	f.Push(c)
-}
-
-func stat(f forth.Forth) {
-	forth.Debug("stat")
-	g := f.Pop().(string)
-	forth.Debug("%v", g)
-	r := f.Pop().(t9.FS)
-	c, err := r.Stat(g)
-	if err != nil {
-		panic(fmt.Sprintf("%v", err))
-	}
-	f.Push(c)
-}
-
-// read up to 8k of data, starting at 0.
-// For now, files are so small that we don't bother with a loop.
-// That's cheap insurance against someone doing something
-// foolish.
-func read(f forth.Forth) {
-	forth.Debug("read")
-	g := f.Pop().(t9.IO)
-	f.Push(g)
-	forth.Debug("%v", g)
-	var b [8192]byte
-	n, err := g.ReadAt(b[:], 0)
-	if err != nil {
-		panic(fmt.Sprintf("%v", err))
-	}
-	f.Push(b[:n])
-}
-
-func toString(f forth.Forth) {
-	forth.Debug("string")
-	g := f.Pop()
-	switch v := g.(type) {
-	case []byte:
-		f.Push(string(v))
-	default:
-		if v, ok := g.(fs.FileInfo); ok {
-			f.Push(fmt.Sprintf("%s: %v, %d bytes", v.Name(), v.Mode(), v.Size()))
-			return
-		}
-		f.Push(fmt.Sprintf("Don't know how to string %T", v))
-	}
-}
 
 func main() {
 	var (
@@ -95,7 +23,6 @@ func main() {
 	)
 
 	flag.Parse()
-	var opts []protocol.ClientOpt
 	if *logger {
 
 	}
@@ -107,32 +34,11 @@ func main() {
 		log.Fatalf("usage: %s ipaddr [commands]", os.Args[0])
 	}
 	addr := a[0]
-	v("Attach: (%q,%q):", *n, addr)
-	root, err := t9.NewNinep(*n, a[0], *aname, opts...)
+	f, err := New(*n, addr, *aname)
 	if err != nil {
 		log.Fatal(err)
 	}
-	v("Attached %v", root)
-	f := forth.New()
-	forth.Debug = log.Printf
-
-	for _, o := range []struct {
-		name string
-		op   forth.Op
-	}{
-		{name: "root", op: func(f forth.Forth) {
-			forth.Debug("root")
-			f.Push(root)
-		},
-		},
-		{name: "open", op: open},
-		{name: "read", op: read},
-		{name: "stat", op: stat},
-		{name: "string", op: toString},
-	} {
-		forth.Putop(o.name, o.op)
-	}
-
+	v("Attach: (%q,%q):", *n, addr)
 	for _, arg := range a[1:] {
 		fmt.Printf("%sOK\n", f.Stack())
 		if err := forth.EvalString(f, arg); err != nil {
