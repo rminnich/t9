5576b35defec376020eb4f98962fffc6a1a44415
Ronald G. Minnich <rminnich@gmail.com>
2023-02-01 16:03:10 -0800

    add creators type and extend NOTES
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- t9/t9.go
+++ t9/t9.go
@@ -20,9 +20,19 @@ type IO interface {
 	WriteAt([]byte, int64) (int, error)
 }
 
+// REGIO is the interface for IO on files.
+type REGIO interface {
+	Read(int64) (uint32, error)
+	Write(int64) (error)
+}
+
 // Closer is for files that can/must be closed.
 type Closer interface {
 	Close() error
 }
+
+type Creator func(FS, IO) error
+
+var creators []Creator
 
 var v = func(string, ...interface{}) {}
